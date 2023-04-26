4dd64f16a4e7a08a0bb884f23c60db6e6b48f7d7
LlamaUnicorn <holidaynight@gmail.com>
2023-04-26 07:05:31 +0300

    01 02

---
+++ ex01.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# 1
+print('Print another line')
+
+# 2
+# print("Hello World!")
+# print("Hello Again")
+# print("I like typing this.")
+# print("This is fun.")
+# print('Yay! Printing.')
+# print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# 3
+# comment
