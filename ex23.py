8f2233ec874e43e3fb4f0454e629256c157fe371
LlamaUnicorn <holidaynight@gmail.com>
2023-05-07 20:44:13 +0300

    ex23-drill-4

--- ex23.py
+++ ex23.py
@@ -13,6 +13,9 @@ def main(language_file, encoding, errors):
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
+    with open('languages_reverse.txt', 'a') as f:
+        f.write(str(raw_bytes))
+        f.write('\n')
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
     print(raw_bytes, "<===>", cooked_string)
