b2b5f787daa0b75d204645a814280ac57e9c7144
LlamaUnicorn <holidaynight@gmail.com>
2023-05-31 07:10:11 +0300

    ex47

---
+++ projects/ex47/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'ex47',
+    'author': 'My Name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['pytest'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'ex47'
+}
