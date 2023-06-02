48f767adb3338be82d5df1b72304b143e8e84f64
LlamaUnicorn <holidaynight@gmail.com>
2023-06-02 06:49:38 +0300

    ex50

--- projects/ex48/tests/lexicon_test.py
+++ projects/ex48/tests/lexicon_test.py
@@ -1,3 +1,13 @@
 import pytest
-from ex48 import Lexicon
+from ex48 import lexicon
+
+def test_directions():
+    assert lexicon.scan("north") == [('direction', 'north')]
+    result = lexicon.scan("north south east")
+    assert result == [
+        ('direction', 'north'),
+        ('direction', 'south'),
+        ('direction', 'east')
+    ]
+
 
